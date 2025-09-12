652b271ac49395093d56120b6a8e07f8593e3716
santoshjadhav12 <santoshkj24@gmail.com>
2025-09-11 21:07:15 -0700

    first-commit

---
+++ src/main/java/com/techsoft/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.techsoft.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/welcome")
+    public String getMessage(){
+        return "Welcome to tech soft";
+        
+    }
+
+}
