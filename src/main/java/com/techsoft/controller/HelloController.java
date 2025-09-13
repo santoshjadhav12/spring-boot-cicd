4930a1c2e2dce3dc08ecb830032d58cd353d8710
santoshjadhav12 <santoshkj24@gmail.com>
2025-09-12 22:42:45 -0700

    final

--- src/main/java/com/techsoft/controller/HelloController.java
+++ src/main/java/com/techsoft/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/welcome")  // Maps requests to the root path "/"
+    @GetMapping("/home")  // Maps requests to the root path "/"
     public String home() {
         return "Welcome to the Spring Boot App on port 9090!";
     }
